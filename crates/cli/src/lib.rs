0c79893c9ac176ff262a095290a4f1981343335a
Manuel Stöger <manuel.stoeger@inso-world.com>
2025-02-10 18:58:19 +0100

    Fixed non commited crates

--- crates/cli/src/lib.rs
+++ crates/cli/src/lib.rs
@@ -1,12 +1,12 @@
 mod commands {
     pub mod cmd;
     pub(crate) mod commits;
-    pub mod blame;
+    // pub mod blame;
     pub mod diff;
     pub mod output_format;
 }
 
 pub use crate::commands::cmd;
 pub use crate::commands::diff;
-pub use crate::commands::blame;
+// pub use crate::commands::blame;
 pub use crate::commands::output_format;
